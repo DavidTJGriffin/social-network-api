4694972e5ff4b0852e910c8be4761f0299fea347
David Griffin <david.dtjg2001@gmail.com>
2022-04-20 16:50:13 -0700

    intial files

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: {
+                validator: () => Promise.resolve(false),
+                message: 'Email validation failed'
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON:
+            { virtuals: true },
+
+            id: false
+    }
+);
+
+//virtual called friendCount
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+const User = model('User', userSchema);
+
+module.exports = User;
