1949056a6a0558db4105a04fd49c2d68df829d50
David Griffin <david.dtjg2001@gmail.com>
2022-05-01 23:20:53 -0700

    added users controllers

---
+++ controllers/user.js
@@ -0,0 +1,58 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+	
+	getUsers(req, res) {
+		User.find({})
+			.select('-__v -thoughts')
+			.then(dbUserData => res.json(dbUserData))
+	},
+
+	
+	getUser({ params }, res) {
+		User.findOne({ _id: params.id })
+			.populate({
+				path: 'thoughts',
+				select: '-__v'
+			})
+			.select('-__v')
+			.then(dbUserData => {
+				res.json(dbUserData);
+			})
+	},
+		
+	createUser({ body }, res) {
+		User.create(body)
+			.then(dbUserData => res.json(dbUserData))
+	},
+
+	updateUser({ params, body }, res) {
+		User.findOneAndUpdate({ _id: params.id }, body)
+			.then(dbUserData => {
+				res.json(dbUserData);
+			})
+	},	
+
+	deleteUser({ params }, res) {
+		User.findOneAndDelete({ _id: params.id })
+			.then(dbUserData => {
+				return Promise.all(dbUserData.thoughts.map(thought => Thought.findOneAndDelete({ _id: thought.toString() })));
+			})
+			
+	},
+	
+	addFriend({ params }, res) {
+		Promise.all([
+			User.findOneAndUpdate(
+				{ _id: params.id },
+				{ $push: { friends: params.friendId } }
+			),
+			User.findOneAndUpdate(
+				{ _id: params.friendId },
+				{ $push: { friends: params.id } }
+			)
+		])
+	},
+};
+
+
